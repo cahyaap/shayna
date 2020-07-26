d871564813d5e220ade239885885f3f2614b76cc
cahya <cahyaajipermana@gmail.com>
2020-07-26 18:12:19 +0700

    first commit

---
+++ src/components/HomePartners.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="partner-logo">
+    <div class="container">
+      <Carousel class="logo-carousel" :loop="true" :nav="false" :items="5" :dots="false" :autoplay="true">
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-1.png" alt />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-2.png" alt />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-3.png" alt />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-4.png" alt />
+          </div>
+        </div>
+        <div class="logo-item">
+          <div class="tablecell-inner">
+            <img src="img/logo-carousel/logo-5.png" alt />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from 'vue-owl-carousel';
+export default {
+    name: 'HomePartners',
+    components: {
+        Carousel
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <HomeSlider />
+    <HomeGallery />
+    <HomePartners />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import HomeSlider from "@/components/HomeSlider.vue";
+import HomeGallery from "@/components/HomeGallery.vue";
+import HomePartners from "@/components/HomePartners.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    HomeSlider,
+    HomeGallery,
+    HomePartners,
+    Footer
+  }
+};
+</script>
